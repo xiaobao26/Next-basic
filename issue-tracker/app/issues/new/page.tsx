e41df128334992bc91de4ce318e358eb608f8534
xiaobao26 <xiaobao.xue@outlook.com>
2024-10-08 10:46:13 +1100

    feat: add Marddown editor in new issue page

--- issue-tracker/app/issues/new/page.tsx
+++ issue-tracker/app/issues/new/page.tsx
@@ -1,18 +1,35 @@
-import { TextField, TextArea, Button } from '@radix-ui/themes'
-import React from 'react'
 
-const page = () => {
+"use client"
+import { TextField, Button } from '@radix-ui/themes'
+import { useEffect, useState } from 'react'
+import dynamic from 'next/dynamic'
+import "easymde/dist/easymde.min.css"
+
+// Dynamically load SimpleMdeReact on the client side only
+// With no SSR
+// To dynamically load a component on the client side, you can use the ssr option to disable server-rendering. This is useful if an external dependency or component relies on browser APIs like window.
+const SimpleMDE = dynamic(() => import('react-simplemde-editor'), { ssr: false });
+
+const Page = () => {
+    const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true); // Ensures this is run only on the client-side
+    }, []);
+
     return (
-        <>  
+        <>
             <div className='max-w-xl space-y-3'>
                 <TextField.Root placeholder='title'>
                     <TextField.Slot />
                 </TextField.Root>
-                <TextArea placeholder="description..." />
+
+                {mounted && <SimpleMDE placeholder="description..." />} {/* Render only after mounting */}
+
                 <Button>Submit New Issue</Button>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default page
+export default Page;
