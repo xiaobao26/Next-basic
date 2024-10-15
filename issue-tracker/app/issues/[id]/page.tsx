f3162656e694ef51536e89a98145c69186840592
xiaobao26 <xiaobao.xue@outlook.com>
2024-10-15 11:13:34 +1100

    refactor: add securing in pages

--- issue-tracker/app/issues/[id]/page.tsx
+++ issue-tracker/app/issues/[id]/page.tsx
@@ -1,16 +1,20 @@
 import prisma from '@/prisma/client';
 import { Box, Flex, Grid } from '@radix-ui/themes';
 import { notFound } from 'next/navigation';
+import { getServerSession } from 'next-auth';
 
 import EditIssueButton from './EditIssueButton';
 import IssueDetails from './IssueDetails';
 import DeleteIssueButton from './DeleteIssueButton';
+import authOptions from '@/app/auth/authOptions';
+
 
 interface Props {
     params: { id: string }
 }
 
 const IssueDetailPage = async ({ params } : Props) => {
+    const session = await getServerSession(authOptions);
 
     const issue = await prisma.issue.findUnique({
         where: {
@@ -28,13 +32,14 @@ const IssueDetailPage = async ({ params } : Props) => {
                 <IssueDetails issue={issue}/>
             </Box>
 
-            <Box className='max-w-xl'>
-                <Flex gap="3" className='flex-col w-1/2'>
-                    <EditIssueButton issueId={issue.id} />
-                    <DeleteIssueButton  issueId={issue.id}/>
-                </Flex>
-                
-            </Box>
+            { session && 
+                <Box className='max-w-xl'>
+                    <Flex gap="3" className='flex-col w-1/2'>
+                        <EditIssueButton issueId={issue.id} />
+                        <DeleteIssueButton  issueId={issue.id}/>
+                    </Flex>
+                </Box> 
+            }
         </Grid>
     )
 }
---
+++ issue-tracker/middleware.ts
@@ -0,0 +1,11 @@
+export { default } from "next-auth/middleware"
+
+export const config = {
+    // *: zero or more
+    // +: one or more
+    // ?: zero or one
+    matcher: [
+        '/issues/new',
+        '/issues/:id*/edit'
+    ]
+}
