d2d24938e9fb52fc92a4a8c8fd1d2eaa58afec28
xiaobao26 <xiaobao.xue@outlook.com>
2024-10-15 11:19:00 +1100

    feat: add secure in post patch delete apis

--- issue-tracker/app/api/issue/[id]/route.ts
+++ issue-tracker/app/api/issue/[id]/route.ts
@@ -1,9 +1,15 @@
+import authOptions from "@/app/auth/authOptions";
 import IssueSchema from "@/app/validationSchema";
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 
 export async function PATCH (request: NextRequest, { params}: { params: { id: string }} ) {
+    const session = await getServerSession(authOptions);
+    if (!session) return NextResponse.json({}, { status: 401 });
+
+
     // check the body validation
     const body = await request.json();
     const validation = IssueSchema.safeParse(body);
@@ -31,6 +37,9 @@ export async function PATCH (request: NextRequest, { params}: { params: { id: st
 }
 
 export async function DELETE(request: NextRequest, { params}: { params: { id: string }}) {
+    const session = await getServerSession(authOptions);
+    if (!session) return NextResponse.json({}, { status: 401 });
+
     // find issue
     const issue = await prisma.issue.findUnique({
         where: {
--- issue-tracker/app/api/issue/route.ts
+++ issue-tracker/app/api/issue/route.ts
@@ -1,10 +1,16 @@
 import prisma from "@/prisma/client";
 import { NextRequest, NextResponse } from "next/server";
 import createIssueSchema from '../../validationSchema';
+import authOptions from "@/app/auth/authOptions";
+import { getServerSession } from "next-auth";
 
 
 
 export async function POST(request: NextRequest) {
+    const session = await getServerSession(authOptions);
+    if (!session) return NextResponse.json({}, { status: 401 });
+
+
     const body = await request.json();
     const validation = createIssueSchema.safeParse(body);
 
